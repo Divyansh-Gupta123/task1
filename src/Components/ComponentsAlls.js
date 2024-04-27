b3912aced8c8c9cd7d7951bc65dd57699995eba3
Divyansh-Gupta123 <divyanshgupta9211@gmail.com>
2024-04-27 11:29:52 +0530

    second

--- src/Components/ComponentsAlls.js
+++ src/Components/ComponentsAlls.js
@@ -10,19 +10,19 @@ const ComponentsAlls = () => {
             
                 <Grid item xs={3} >
                 <div style={{height:'400px',width:'100%',background:'#95a5a6',display:'flex',textAlign:'center',justifyContent:'center',fontSize:20,fontWeight:'bold',border:'0.5px solid black'}}>
-                 <div style={{marginTop:150}}> FILE TREE  <h6> please daprop the file on here.....</h6> </div>
+                 <div style={{marginTop:150}}> FILE TREE   </div>
                 
                 </div>
                 
             </Grid>
             <Grid item xs={9} >
             <div style={{height:'400px',width:'100%',background:'#95a5a6',display:'flex',textAlign:'center',justifyContent:'center',fontSize:20,fontWeight:'bold',border:'0.5px solid black'}}>
-            <div style={{marginTop:150}}>  TERMINAL...... <h6> it a terminal for any execution</h6></div>
+            <div style={{marginTop:150}}>  TERMINAL...... </div>
            </div>
             </Grid>
             <Grid item xs={12} >
             <div style={{height:'400px',width:'100%',background:'#95a5a6',display:'flex',textAlign:'center',justifyContent:'center',fontSize:20,fontWeight:'bold',border:'0.5px solid black'}}>
-            <div style={{marginTop:150}}>    EDITOR.......<h6> it a editor and please type heret </h6></div>
+            <div style={{marginTop:150}}>    EDITOR.......</div>
            </div>
             </Grid>
           
